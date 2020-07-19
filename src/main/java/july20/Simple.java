88223c1e2bdafe947dd12fcf6d6ceee0bfa55f0f
nandhinipriya0725 <nandhinipriyapandiyan@gmail.com>
2020-07-19 17:46:43 +0530

    My project design

---
+++ src/main/java/july20/Simple.java
@@ -0,0 +1,16 @@
+package july20;
+
+public class Simple {
+
+
+	public void printStmt() {
+		System.out.println("Hi");
+	}
+
+	public static void main(String[] args) {
+		System.out.println("HelloWorld");
+		Simple s=new Simple();
+		s.printStmt();
+	}
+
+}
