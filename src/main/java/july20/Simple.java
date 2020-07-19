fc9c0ed1c3cc9831d18a968f99d1d477e6058dc1
nandhinipriya0725 <nandhinipriyapandiyan@gmail.com>
2020-07-19 18:25:41 +0530

    One More Method Added

--- src/main/java/july20/Simple.java
+++ src/main/java/july20/Simple.java
@@ -7,10 +7,17 @@ public class Simple {
 		System.out.println("Hi");
 	}
 
+	public int add(int a,int b) {
+
+		int c=a+b;
+		return c;
+	}
+
 	public static void main(String[] args) {
 		System.out.println("HelloWorld");
 		Simple s=new Simple();
 		s.printStmt();
+		System.out.println(s.add(5,10));
 	}
 
 }
